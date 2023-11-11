cf31c94c94142bb0c539c9d49ce3565c6dd144be
ffex <kurogan21@gmail.com>
2023-11-11 08:51:02 +0100

    added proj with hibernate reactive

---
+++ demo-repo-hibernate-reactive/src/main/java/com/example/demorepohibernatereactive/DemoRepoHibernateReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorepohibernatereactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRepoHibernateReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRepoHibernateReactiveApplication.class, args);
+	}
+
+}
