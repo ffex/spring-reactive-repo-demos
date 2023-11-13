943351e0e4a475059a8701fadb60aa2613a89553
ffex <kurogan21@gmail.com>
2023-11-13 11:42:04 +0100

    activate hibernate Reactive + some fix for the example DB

--- demo-repo-hibernate-reactive/src/main/java/com/example/demorepohibernatereactive/DemoRepoHibernateReactiveApplication.java
+++ demo-repo-hibernate-reactive/src/main/java/com/example/demorepohibernatereactive/DemoRepoHibernateReactiveApplication.java
@@ -1,7 +1,10 @@
 package com.example.demorepohibernatereactive;
 
+import jakarta.persistence.Persistence;
+import org.hibernate.reactive.mutiny.Mutiny;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoRepoHibernateReactiveApplication {
@@ -9,5 +12,12 @@ public class DemoRepoHibernateReactiveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoRepoHibernateReactiveApplication.class, args);
 	}
+
+	@Bean
+	public Mutiny.SessionFactory sessionFactory() {
+		return Persistence.createEntityManagerFactory("aziendaTest")
+				.unwrap(Mutiny.SessionFactory.class);
+	}
+
 
 }
